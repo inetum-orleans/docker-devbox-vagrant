edd91c4cdabb4ecae38e8548906ea2e6efcb787d
Rémi Alvergnat <remi.alvergnat@gfi.fr>
2017-07-03 11:32:24 +0200

    Meilleur affichage lors du téléchargement de docker-compose

--- provision/21-docker-compose.sh
+++ provision/21-docker-compose.sh
@@ -2,7 +2,7 @@
 
 echo "## Installation de Docker-Compose"
 DOCKER_COMPOSE_VERSION=1.13.0
-curl -L https://github.com/docker/compose/releases/download/$DOCKER_COMPOSE_VERSION/docker-compose-`uname -s`-`uname -m` > ./docker-compose
+curl -fsSL -o ./docker-compose https://github.com/docker/compose/releases/download/$DOCKER_COMPOSE_VERSION/docker-compose-`uname -s`-`uname -m`
 sudo mv ./docker-compose /usr/local/bin/docker-compose
 
 sudo chmod +x /usr/local/bin/docker-compose
