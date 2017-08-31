59f9e2bc074882ba9d50a5fcacc907728f6f2849
Rémi Alvergnat <remi.alvergnat@gfi.fr>
2017-08-31 08:08:39 +0200

    Utilisation de la dernière version de docker-compose

--- provision/21-docker-compose.sh
+++ provision/21-docker-compose.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-DOCKER_COMPOSE_VERSION=1.13.0
+DOCKER_COMPOSE_VERSION=$(curl -s https://api.github.com/repos/docker/compose/releases/latest | grep 'tag_name' | cut -d\" -f4)
 docker-compose --version | grep "docker-compose version $DOCKER_COMPOSE_VERSION"
 VERSION_MATCH=$?
 if [ "$VERSION_MATCH" != "0" ]; then
@@ -9,6 +9,8 @@ if [ "$VERSION_MATCH" != "0" ]; then
     mv ./docker-compose /usr/local/bin/docker-compose
 
     chmod +x /usr/local/bin/docker-compose
+
+    curl -sL https://raw.githubusercontent.com/docker/compose/${DOCKER_COMPOSE_VERSION}/contrib/completion/bash/docker-compose > /etc/bash_completion.d/docker-compose
 else
     echo "## docker-compose $DOCKER_COMPOSE_VERSION est déjà installé"
 fi
