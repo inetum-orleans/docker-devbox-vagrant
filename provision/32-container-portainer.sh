d4783bb22eafa3284061d7cfee158e657b55f5f1
frup67419 <rodolphe.habegger@gfi.fr>
2017-11-21 08:43:35 +0100

    add restart unless-stopped and named container in provision

--- provision/32-container-portainer.sh
+++ provision/32-container-portainer.sh
@@ -3,7 +3,13 @@
 if [ ! "$(docker ps -a | grep portainer)" ]; then
     echo "## Création du container portainer"
 
-    docker run -d -p 9000:9000 -v /var/run/docker.sock:/var/run/docker.sock portainer/portainer -H unix:/ //var/run/docker.sock --no-auth
+    docker run -d -p 9000:9000 \
+        --restart unless-stopped \
+        --name portainer \
+        -v /var/run/docker.sock:/var/run/docker.sock \
+        portainer/portainer \
+        -H unix:///var/run/docker.sock \
+        --no-auth
 else
     echo "## Le container portainer existe déja."
 fi
