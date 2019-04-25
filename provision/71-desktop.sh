2266adad861d51ad8e136d7260e63250ae0a4ca1
Rémi Alvergnat <remi.alvergnat@gfi.fr>
2019-04-25 14:54:29 +0200

    Add autologin, disable screen saver lock, and and languages packages

--- provision/71-desktop.sh
+++ provision/71-desktop.sh
@@ -4,6 +4,12 @@ export DEBIAN_FRONTEND=noninteractive
 echo "## Installing desktop environement"
 sudo apt-get install -fy kubuntu-desktop
 
+kwriteconfig5 --file ~/.config/kscreenlockerrc --group Daemon --key Autolock --type bool false
+kwriteconfig5 --file ~/.config/kscreenlockerrc --group Daemon --key LockOnResume --type bool false
+sudo kwriteconfig5 --file /etc/sddm.conf --group Autologin --key User --type string vagrant
+
+sudo apt-get -y install `check-language-support -l $CONFIG_LANGUAGE`
+
 if (dpkg -l avahi-daemon); then
   # avahi-daemon may cause issues with .local domains
   sudo systemctl disable avahi-daemon
