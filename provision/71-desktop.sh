61fa3d33984e2e33385f29448e7fe978c737f39b
Rémi Alvergnat <remi.alvergnat@gfi.fr>
2019-04-26 15:57:38 +0200

    Automate installation of custom CA certificates to Firefox

--- provision/71-desktop.sh
+++ provision/71-desktop.sh
@@ -22,3 +22,54 @@ fi
 sudo DEBIAN_FRONTEND=noninteractive apt-get install -qy virtualbox-guest-dkms virtualbox-guest-x11
 #sudo apt-get purge -fy avahi-daemon
 #sudo apt-get install -fy resolvconf
+
+echo "## Installing Firefox policies for CA certificates"
+
+sudo mkdir -p "/usr/lib/mozilla"
+sudo ln -sfn /usr/local/share/ca-certificates "/usr/lib/mozilla/certificates"
+
+sudo tee /usr/lib/mozilla/update-firefox-certificates.py >/dev/null <<EOF
+#!/usr/bin/env python3
+# -*- coding: utf-8
+import os, json
+
+policies_fp = "/usr/lib/firefox/distribution/policies.json"
+certificates_dir = "/usr/lib/mozilla/certificates"
+
+data = None
+if not os.path.exists(policies_fp):
+    open(policies_fp, 'a').close()
+with open(policies_fp, "r", encoding="utf-8") as json_file:
+    try:
+        data = json.load(json_file)
+    except:
+        data = {}
+
+    try:
+        certs = data['policies']['Certificates']['Install']
+    except KeyError:
+        if 'policies' not in data:
+            data = {'policies': {}}
+        if 'Certificates' not in data['policies']:
+            data['policies']['Certificates'] = {"ImportEnterpriseRoots": True}
+        if 'Install' not in data['policies']['Certificates']:
+            certs = []
+            data['policies']['Certificates']['Install'] = certs
+
+    print("Installing certificates from %s" % certificates_dir)
+    for f in os.listdir(certificates_dir):
+        print("Certificate found: %s" % f)
+        if f not in certs:
+            certs.append(f)
+
+with open(policies_fp, "w", encoding="utf-8") as json_file:
+    json.dump(data, json_file, ensure_ascii=False, indent=4)
+    
+print("Policies files written to %s" % policies_fp)
+EOF
+sudo chmod +x /usr/lib/mozilla/update-firefox-certificates.py
+sudo /usr/lib/mozilla/update-firefox-certificates.py
+
+# TODO: Setup update-firefox-certificates.py as a systemd oneshot service
+# TODO: Maybe this script should be removed after fixing the issue https://github.com/mozilla/policy-templates/issues/291
+# TODO: After the issue is release, ImportEnterpriseRoots should be enough
