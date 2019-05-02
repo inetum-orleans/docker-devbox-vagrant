7badea53dbe829c5b897687e93b6f717163170fc
Rémi Alvergnat <remi.alvergnat@gfi.fr>
2019-05-02 09:59:12 +0200

    Fix usermod -aG sudo vagrant

--- provision/01-prepare.sh
+++ provision/01-prepare.sh
@@ -11,7 +11,7 @@ sudo apt-get update
 echo "## Préparation"
 mkdir -p /home/$USER/.provision
 
-sudo groupadd -aG sudo vagrant
+sudo usermod -aG sudo vagrant
 
 sudo mkdir -p /usr/share/ca-certificates/private
 sudo chmod go+xr /usr/share/ca-certificates/private
