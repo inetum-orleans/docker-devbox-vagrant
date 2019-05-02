a9f14c3e94a1660acaccc0cba59b080feb28f497
Rémi Alvergnat <remi.alvergnat@gfi.fr>
2019-05-02 10:43:26 +0200

    Remove certutil command from desktop additions

--- provision/79-desktop-additions.sh
+++ provision/79-desktop-additions.sh
@@ -13,5 +13,3 @@ wget -q -O - https://dl-ssl.google.com/linux/linux_signing_key.pub | sudo apt-ke
 sudo sh -c 'echo "deb http://dl.google.com/linux/chrome/deb/ stable main" >> /etc/apt/sources.list.d/google-chrome.list'
 sudo apt-get update
 sudo apt-get install -fy google-chrome-stable
-
-certutil -d sql:$HOME/.pki/nssdb -A -t "C,," -i /usr/local/share/ca-certificates/fwca.annuaire.groupe.local.crt -n fwca.annuaire.groupe.local.crt
