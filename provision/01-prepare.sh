8b2cdf0b5475e008223cd86076e51ae718f70155
Rémi Alvergnat <remi.alvergnat@gfi.fr>
2019-05-06 16:28:09 +0200

    Setup inotify watchers limit

--- provision/01-prepare.sh
+++ provision/01-prepare.sh
@@ -43,5 +43,8 @@ sudo service dnsmasq restart
 # Disable systemd-resolved by adding fallback resolv.conf file to resolvconf tail
 sudo ln -fs /run/systemd/resolve/resolv.conf /etc/resolvconf/resolv.conf.d/tail
 
+# inotify limit
+echo fs.inotify.max_user_watches=524288 | sudo tee -a /etc/sysctl.conf && sudo sysctl -p && sudo sysctl --system
+
 sudo resolvconf --enable-updates
 sudo resolvconf -u
