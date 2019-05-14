c18cbb1140d628fbbd510deb5840503cd32f5730
Rémi Alvergnat <remi.alvergnat@gfi.fr>
2019-05-14 08:29:59 +0200

    Use sudo to install cfssl-cli

--- provision/07-cfssl-cli.sh
+++ provision/07-cfssl-cli.sh
@@ -3,6 +3,6 @@ CFSSL_CLI_VERSION=$(curl -s https://api.github.com/repos/Toilal/python-cfssl-cli
 echo "## Installation de cfssl-cli $CFSSL_CLI_VERSION"
 
 curl -sL -o ./cfssl-cli https://github.com/Toilal/python-cfssl-cli/releases/download/$CFSSL_CLI_VERSION/cfssl-cli
-mv ./cfssl-cli /usr/local/bin/cfssl-cli
+sudo mv ./cfssl-cli /usr/local/bin/cfssl-cli
 
-chmod +x /usr/local/bin/cfssl-cli
+sudo chmod +x /usr/local/bin/cfssl-cli
