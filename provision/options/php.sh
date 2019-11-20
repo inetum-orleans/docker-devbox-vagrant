1271ef1773e68093e92e71a8e5921d655f65123a
Rémi Alvergnat <toilal.dev@gmail.com>
2019-11-20 22:38:30 +0100

    Add missing dependency for PHP (ssl)

--- provision/options/php.sh
+++ provision/options/php.sh
@@ -2,7 +2,7 @@ if [[ ! -d "$HOME/.phpenv" ]]; then
   curl -L https://raw.githubusercontent.com/phpenv/phpenv-installer/master/bin/phpenv-installer \  | bash
 
   # PHPEnv configuration
-  export PHPENV_ROOT="/home/vagrant/.phpenv"
+  export PHPENV_ROOT="$HOME/.phpenv"
   export PATH="$HOME/.phpenv/bin:${PATH}"
   eval "$(phpenv init -)"
 
@@ -10,20 +10,22 @@ if [[ ! -d "$HOME/.phpenv" ]]; then
 
   cat <<'EOF' >>"$HOME/.bashrc"
 # PHPEnv configuration
-export PHPENV_ROOT="/home/vagrant/.phpenv"
+export PHPENV_ROOT="$HOME/.phpenv"
 export PATH="$HOME/.phpenv/bin:${PATH}"
 eval "$(phpenv init -)"
 EOF
 
   sudo apt-get install -y autoconf dpkg-dev file g++ gcc libc-dev make pkg-config re2c \
-    libxml2-dev libcurl4-openssl-dev libjpeg-dev libtidy-dev libxslt1-dev libzip-dev unzip
+    libxml2-dev libcurl4-openssl-dev libjpeg-dev libtidy-dev libxslt1-dev libzip-dev \
+    libssl-dev unzip
 
-  # Some module may require additional libs, like libpq-dev for pdo_pgsql, ...
+  # Some module may require additional libs and configuration through PHP_BUILD_CONFIGURE_OPTS environment variable,
+  # like libpq-dev for pdo_pgsql (PHP_BUILD_CONFIGURE_OPTS="--with-pdo_pgsql=shared")
 
   phpenv latest install -s
   phpenv latest global
 else
-  export PHPENV_ROOT="/home/vagrant/.phpenv"
+  export PHPENV_ROOT="$HOME/.phpenv"
   export PATH="$HOME/.phpenv/bin:${PATH}"
   eval "$(phpenv init -)"
 
