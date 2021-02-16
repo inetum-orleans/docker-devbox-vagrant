26e779230fea31e7718b0896f0a781be5569e451
Rémi Alvergnat <toilal.dev@gmail.com>
2021-02-16 22:26:43 +0100

    fix(docker-devbox): add DOCKER_DEVBOX_LEGACY=1 flag

--- provision/31-docker-devbox.sh
+++ provision/31-docker-devbox.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 echo "## Installation de docker-devbox"
-curl -sL https://github.com/gfi-centre-ouest/docker-devbox/raw/master/installer | bash
+curl -sL https://github.com/gfi-centre-ouest/docker-devbox/raw/master/installer | DOCKER_DEVBOX_LEGACY=1 bash
 
 mkdir -p "$HOME/projects"
