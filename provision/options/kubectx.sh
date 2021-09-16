8e5501ebd253c804be946b18012fcd6ca574f1b5
lcube <info@lcube.fr>
2021-09-16 10:56:49 +0200

    correction typo

--- provision/options/kubectx.sh
+++ provision/options/kubectx.sh
@@ -4,4 +4,4 @@ echo "## Installing kubectx"
 
 curl -LO https://github.com/ahmetb/kubectx/releases/download/v0.9.3/kubectx
 chmod +x ./kubectx
-sudo mv ./kubectl /usr/local/bin/kubectx
+sudo mv ./kubectx /usr/local/bin/kubectx
