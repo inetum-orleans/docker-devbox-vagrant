201eff238c829da1835d99838a0d51eeb8aab67c
Jérémy VIGNELLES <jeremy.vignelles@inetum.com>
2023-01-31 18:38:48 +0100

    ddb: remove usage of older glibc, using the main branch now

--- provision/31-docker-devbox.sh
+++ provision/31-docker-devbox.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 echo "## Installation de docker-devbox"
-curl -sL https://github.com/gfi-centre-ouest/docker-devbox/raw/master/installer | DOCKER_DEVBOX_LEGACY=1 DOCKER_DEVBOX_DDB_ASSET_NAME=ddb-linux-older-glibc bash
+curl -sL https://github.com/gfi-centre-ouest/docker-devbox/raw/master/installer | bash
 
 mkdir -p "$HOME/projects"
