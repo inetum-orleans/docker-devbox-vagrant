dc6c5954c7e013c84f44fe176e0246dee0d985c3
Jérémy VIGNELLES <jeremy.vignelles@inetum.com>
2023-01-31 18:40:09 +0100

    Allowed to configure the az cli login options

--- provision/options/azure-cli.sh
+++ provision/options/azure-cli.sh
@@ -7,3 +7,12 @@ curl -sL https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor | sud
 echo "deb [arch=amd64] https://packages.microsoft.com/repos/azure-cli/ $(lsb_release -cs) main" | sudo tee /etc/apt/sources.list.d/azure-cli.list
 
 sudo apt-get update && sudo apt-get install azure-cli
+
+# If the azure username is set
+if [ -n "$AZ_USERNAME" ]; then
+  az login --service-principal --username $AZ_USERNAME -p "$AZ_PASSWORD" --tenant $AZ_TENANT
+  # Test if user crontab contains `az acr login --name gfiorleans`. If not, add it.
+  if ! crontab -l | grep -q "az acr login --name gfiorleans"; then
+    (crontab -l 2>/dev/null; echo "0,15,30,45 * * * * az acr login --name gfiorleans") | crontab -
+  fi
+fi
