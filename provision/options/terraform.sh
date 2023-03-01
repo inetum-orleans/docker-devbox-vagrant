d534d657288ff6ebb99a1e78eba504b8a9f94fd7
Laurent LE LARDEUX <laurent.le-lardeux@inetum.com>
2023-03-01 11:14:21 +0100

    fix: remove apt upgrade from script

--- provision/options/terraform.sh
+++ provision/options/terraform.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 echo "## Installing Terraform"
-sudo apt update && apt upgrade -y
+sudo apt update
 sudo apt install -y gnupg software-properties-common curl
 curl -fsSL https://apt.releases.hashicorp.com/gpg | sudo apt-key add -
 sudo apt-add-repository "deb [arch=amd64] https://apt.releases.hashicorp.com $(lsb_release -cs) main"
